9770846c7153dcb19cf88217755650659eedd8cb
genesCode <Genesman@users.noreply.github.com>
2021-12-28 16:11:29 -0300

    Avance en sección modelos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-/*  Swiper  */
+/*  Banner Slide  */
 var swiperBannerThumbs = new Swiper(".banner-slide-thumbs", {
     spaceBetween: 0,
     slidesPerView: 3,
@@ -24,3 +24,36 @@ var swiperBanner = new Swiper(".banner-slide", {
         swiper: swiperBannerThumbs,
     },
 });
+
+/* Banner Models */
+var swiperModelThumbs = new Swiper(".model-slide-thumbs", {
+    spaceBetween: 0,
+    slidesPerView: 1,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    },
+});
+var swiperModel = new Swiper(".model-slide-features", {
+    loop: false,
+    speed: 1000,
+    spaceBetween: 20,
+    grabCursor: false,
+    watchSlidesProgress: true,
+    mousewheelControl: true,
+    keyboardControl: true,
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+    thumbs: {
+        swiper: swiperModelThumbs,
+    },
+});
+
+/* Lightgallery */
+$( '.swipebox' ).swipebox();
